be5e62d2665b4c5bde17f88689c6d26576d5f785
weiliang <welian0006@163.com>
2017-02-06 21:24:23 +0800

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -64,7 +64,7 @@ HerokuBlog001::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'entries#index'
+  #root :to => 'entries#index'
 
   # See how all your routes lay out with "rake routes"
 
